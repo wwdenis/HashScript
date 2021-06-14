4beb52da1d4252be8f4da7b4af6a1accea7240db
Denis Da Silva <denis_silva@live.com>
2021-06-14 23:18:06 +0200

    [Dot] New TokenType

--- src/HashScript/Domain/TokenType.cs
+++ src/HashScript/Domain/TokenType.cs
@@ -11,6 +11,7 @@ namespace HashScript.Domain
         Space = ' ',
         Hash = '#',
         Complex = '+',
+        Dot = '.',
         Condition = '?',
         Negate = '!',
         Content = '$',
