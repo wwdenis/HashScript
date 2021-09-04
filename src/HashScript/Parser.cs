82b459bb9d9249bf6f735b65df14b48512901b0f
Denis Da Silva <denis_silva@live.com>
2021-09-04 22:54:47 +0200

    Simple Field parsing

--- src/HashScript/Parser.cs
+++ src/HashScript/Parser.cs
@@ -21,6 +21,7 @@ namespace HashScript
 
         public DocumentNode Parse()
         {
+            var errors = new List<string>();
             var nodes = new List<Node>();
             var content = this.lexer.ReadAll();
             var tokens = new Queue<Token>(content);
@@ -29,7 +30,7 @@ namespace HashScript
 
             while (tokens.Any())
             {
-                if (TryParseField(tokens, out var fieldNode))
+                if (TryParseField(tokens, errors, out var fieldNode))
                 {
                     nodes.Add(fieldNode);
                 }
@@ -43,34 +44,105 @@ namespace HashScript
                 }
             }
 
-            var doc = new DocumentNode(nodes);
+            var doc = new DocumentNode(nodes, errors);
 
             return doc;
         }
 
-        private bool TryParseField(Queue<Token> tokens, out FieldNode node)
+        private bool TryParseField(Queue<Token> tokens, List<string> errors, out FieldNode node)
         {
-            node = null;
-            return false;
-        }
+            if (!NextContains(tokens, NodeType.Field))
+            {
+                node = null;
+                return false;
+            }
 
-        private bool TryParseText(Queue<Token> tokens, out TextNode node)
-        {
-            var queue = new Queue<Token>();
-            var content = string.Empty;
+            var buffer = new Queue<Token>();
+
+            var name = string.Empty;
+            var error = string.Empty;
+            Token invalid = null;
+            
+            var hasOpened = false;
+            var hasClosed = false;
 
             while (tokens.Any())
             {
                 var current = tokens.Dequeue();
-                var type = MapType(current);
-                
-                if (type != NodeType.Text)
+
+                if (current.Type == TokenType.Hash)
                 {
-                    content = BuildTextContent(queue);
+                    if (hasOpened)
+                    {
+                        hasClosed = true;
+                    }
+                    else
+                    {
+                        hasOpened = true;
+                    }
+                }
+                else if (current.Type == TokenType.Text)
+                {
+                    if (HasValidName(current))
+                    {
+                        buffer.Enqueue(current);
+                    }
+                    else
+                    {
+                        invalid = current;
+                    }
+                }
+                else
+                {
+                    invalid = current;
+                }
+                
+                if (invalid is not null || hasClosed)
+                {
+                    break;   
+                }
+            }
+
+            if (invalid is not null)
+            {
+                error = $"Field contains an invalid character: {GetTokenContent(invalid)}";
+            }
+            else if (!buffer.Any())
+            {
+                error = "Field must contain a valid name";
+            }
+            else if (!hasClosed)
+            {
+                error = "Field does not contains a close Hash";
+            }
+
+            if (!string.IsNullOrWhiteSpace(error))
+            {
+                errors.Add(error);
+                node = null;
+                return false;
+            }
+
+            name = BuildContent(buffer);
+            node = new FieldNode(name);
+            return true;
+        }
+
+        private bool TryParseText(Queue<Token> tokens, out TextNode node)
+        {
+            var buffer = new Queue<Token>();
+            var content = string.Empty;
+
+            while (tokens.Any())
+            {
+                if (!NextContains(tokens, NodeType.Text))
+                {
+                    content = BuildContent(buffer);
                     break;
                 }
 
-                queue.Enqueue(current);
+                var current = tokens.Dequeue();
+                buffer.Enqueue(current);
             }
 
             if (string.IsNullOrEmpty(content))
@@ -83,19 +155,33 @@ namespace HashScript
             return true;
         }
 
-        private static NodeType MapType(Token token)
+        private static bool NextContains(Queue<Token> tokens, NodeType expectedType)
         {
-            return token.Type switch 
+            var mappings = new Dictionary<TokenType, NodeType> 
             {
-                TokenType.Text  => NodeType.Text,
-                TokenType.Space => NodeType.Text,
-                TokenType.NewLine => NodeType.Text,
-                TokenType.EOF => NodeType.None,
-                _ => throw new ArgumentException($"Invalid TokenType: {token.Type}", nameof(token)),
+                { TokenType.Text, NodeType.Text },
+                { TokenType.Space, NodeType.Text },
+                { TokenType.Tab, NodeType.Text },
+                { TokenType.NewLine, NodeType.Text },
+                { TokenType.Hash, NodeType.Field },
             };
+
+            if (!tokens.Any())
+            {
+                return false;
+            }
+
+            var next = tokens.Peek();
+
+            if (mappings.TryGetValue(next.Type, out var nodeType))
+            {
+                return nodeType == expectedType;
+            }
+
+            return false;
         }
 
-        private static string BuildTextContent(Queue<Token> tokens)
+        private static string BuildContent(Queue<Token> tokens)
         {
             if (!tokens.Any())
             {
@@ -107,6 +193,26 @@ namespace HashScript
                              let content = string.IsNullOrEmpty(i.Content) ? new string(tokenChar, i.Length) : i.Content
                              select content;
             return string.Join("", allContent);
+        }
+
+        private string GetTokenContent(Token token)
+        {
+            var type = token.Type;
+            
+            return type switch
+            {
+                TokenType.Space => type.ToString(),
+                TokenType.Tab => type.ToString(),
+                TokenType.NewLine => type.ToString(),
+                TokenType.EOF => type.ToString(),
+                TokenType.Text => token.Content,
+                _ => $"{(char)type}"
+            };
+        }
+
+        private bool HasValidName(Token token)
+        {
+            return token.Content?.All(i => Char.IsLetterOrDigit(i)) ?? false;
         }
     }
 }
