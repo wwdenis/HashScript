acabdc35f32a444fd3c91566cf0c2dd24ad6f2c3
Denis Da Silva <denis_silva@live.com>
2021-09-04 22:55:28 +0200

    Simple Field parsing tests

--- tests/HashScript.Tests/ParserTests.cs
+++ tests/HashScript.Tests/ParserTests.cs
@@ -8,7 +8,24 @@ namespace HashScript.Tests
     {
         [Theory]
         [FileData("ParseText")]
+        [FileData("ParseField")]
         public void Can_Parse_Success(string template, FakeNode expected)
+        {
+            var subject = new Parser(template);
+            var result = subject.Parse();
+
+            result
+                .Should()
+                .BeEquivalentTo(
+                    expected,
+                    opts => opts
+                        .ExcludingMissingMembers()
+                        .IgnoringCyclicReferences());
+        }
+
+        [Theory]
+        [FileData("ParseFieldError")]
+        public void Can_Parse_Errors(string template, FakeNode expected)
         {
             var subject = new Parser(template);
             var result = subject.Parse();
